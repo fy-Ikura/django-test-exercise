22be96a1335c17ea77fe9c7f70af9ee5cd697e55
1f10250139 <s1f102501394@iniad.org>
2026-06-25 12:00:45 +0900

    flake8の修正4

--- todo/tests.py
+++ todo/tests.py
@@ -65,7 +65,7 @@ class TodoviewTestCase(TestCase):
         client = Client()
         data = {'title': 'Test Task', 'due_at': '2024-06-30 23:59:59'}
         response = client.post('/', data)
-   
+
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, 'todo/index.html')
         self.assertEqual(len(response.context['tasks']), 1)
